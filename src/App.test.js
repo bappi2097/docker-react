79beaea2d5cab8b6d9e764ab6afc5ab6ae8e38c2
Bappi Saha <bappi35-2097@diu.edu.bd>
2023-06-15 22:46:06 +0600

    fix: this is error fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders It\'s amazing link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/It's amazing/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 
 test('new test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/It's amazing/i);
   expect(linkElement).toBeInTheDocument();
 });
 
